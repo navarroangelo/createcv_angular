56dd24b6775ff7465d112090ce4dcf4a29cd298f
Angelo Navarro <marcelxangelo@gmail.com>
2024-11-26 22:45:19 +0800

    Initial Commit, SubComponent In-Progess

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { EducationComponent } from './education/education.component';
+import { TrainingComponent } from './training/training.component';
+import { ReferencesComponent } from './references/references.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    PersonalInfoComponent,
+    WorkExperienceComponent,
+    EducationComponent,
+    TrainingComponent,
+    ReferencesComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'prelim_lab_2';
